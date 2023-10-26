586cc93f43ac63e4bc153e903cd30209ee802d48
Ram-Ram007 <ramajayam2411@gmail.com>
2023-10-26 22:50:27 +0530

    first commit

---
+++ app/src/pages/Edit.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import Layout from "../components/layout";
+
+function Edit() {
+  const { id } = useParams();
+
+  useEffect(() => {
+    console.log("Getting info of ", id);
+  }, [id]);
+
+  return (
+    <>
+      
+        <h1>Edit Form</h1>
+        <form>
+          <label>Enter Alter Name</label>
+          <input type="text"></input>
+          <label>Enter Alter Year</label>
+          <input type="text"></input>
+        </form>
+
+        <Link to="/">
+          <button>save </button>
+        </Link>
+      
+    </>
+  );
+}
+
+export default Edit;
